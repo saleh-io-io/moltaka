b0c37882e68c705736e4c6107478f31e1f9a677c
Saleh Al Saleh <saleh.alsaleh802@gmail.com>
2025-05-30 23:04:01 +0300

    commenting out login functionality

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -7,7 +7,7 @@ return (
         <div>
         {/* signin with github button */}
         <button 
-            onClick={() => window.location.href = '/auth/github'}
+            // onClick={() => window.location.href = '/auth/github'}
             className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors">
 
         <h3 className="text-2xl font-bold mb-4">Sign In with github</h3>
